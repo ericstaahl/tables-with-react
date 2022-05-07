e02e7074051afbe38570447627c4e87f6350855f
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2022-05-07 13:53:50 +0200

    Only able to access 'HomePage' when the correct 'username' and 'password' has been entered.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,29 +1,69 @@
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 
 const LoginPage = () => {
+  const navigate = useNavigate()
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [success, setSucess] = useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    if (username === 'username' && password === 'password') {
+      navigate('/home')
+    }    
   }
+
+  console.log(username)
+  console.log(password)
+
+  useEffect(() => {
+
+  }, [username, password])
 
   return (
     <>
       <div className="form-wrapper">
         <form className="flex-container" onSubmit={handleSubmit}>
+
           <div className="form-elements-wrapper">
             <label htmlFor="username">Username</label>
-            <input type="text" name="username" placeholder="Username" />
+
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value)
+              }}
+            />
           </div>
+
           <div className="form-elements-wrapper">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" placeholder="Password" />
+
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value)
+              }}
+            />
+
           </div>
           <div className="form-elements-wrapper">
-            <Link to="/home">
               <button className="standard-button">Login</button>
-            </Link>
           </div>
         </form>
+
+        {success && (
+          <div>
+            Success!
+          </div>
+        )}
       </div>
     </>
   )
